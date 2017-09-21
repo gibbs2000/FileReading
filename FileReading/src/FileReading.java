0d27521ff76a971fd4b1abab3bef6974a8715a34
srgib <gibbonss@bishopireton.org>
2017-09-20 23:24:06 -0400

    final work on 9/20

--- FileReading/src/FileReading.java
+++ FileReading/src/FileReading.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileReading {
@@ -10,7 +11,7 @@ public class FileReading {
 	 */
 	public static void main(String[] args) {
 		// Checks if there is enough files as args
-		if (args.length < 4) {
+		if (args.length < 3) {
 			System.out.println("You did not input enough files");
 			System.exit(1);
 		}
@@ -42,6 +43,20 @@ public class FileReading {
 		out.println();
 
 		// Step 5: Storytime
+
+		// If less than 4 command line args are input, asks for user-submitted words to
+		// complete story
+		if (args.length < 4) {
+			out.println(writeStoryUser(file3));
+		}
+
+		// If 4 or more command line args are supplied, uses arg[3] to replace words in
+		// story
+		if (args.length > 4) {
+			Scanner file4 = fileToScanner(args[3], 4);
+			writeStoryArg(file3, file4);
+			file4.close();
+		}
 
 		// Closes all open files
 		file1.close();
@@ -101,19 +116,17 @@ public class FileReading {
 
 		// Loops while there are lines left in the Scanner to be checked
 		while (numBraces >= 0 && toBeChecked.hasNextLine()) {
-			if (toBeChecked.hasNext("\\{")) {
+			String line = toBeChecked.nextLine();
+			if (line.contains("{")) {
 				numBraces++;
 			}
-			if (toBeChecked.hasNext("\\}")) {
+			if (line.contains("}")) {
 				numBraces--;
 
 			}
-			toBeChecked.nextLine();
 
 		}
-		if (numBraces != 0)
-			return false;
-		return true;
+		return numBraces == 0;
 
 	}
 
@@ -123,9 +136,53 @@ public class FileReading {
 	 * @return
 	 */
 	public static boolean compareScanners(Scanner f1, Scanner f2) {
-		String s1 = f1.toString();
-		String s2 = f2.toString();
-		return s1.equals(s2);
+		while (f1.hasNextLine() && f2.hasNextLine()) {
+			String l1 = f1.nextLine();
+			String l2 = f2.nextLine();
+			for (int i = 0; i < l1.length(); i++) {
+				if (!(l1.charAt(i) == l2.charAt(i))) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
+	public static String writeStoryUser(Scanner file) {
+		ArrayList<String> words = new ArrayList<String>();
+		Scanner kb = new Scanner(System.in);
+
+		while (file.hasNextLine()) {
+			String line = file.nextLine();
+
+			if (line.contains("<") && line.contains(">")) {
+
+				String type = line.substring(line.indexOf("<") + 1, line.indexOf(">"));
+
+				System.out.println("Insert a " + type);
+
+				String userInput = kb.nextLine();
+				words.add(userInput);
+
+			}
+		}
+		StringBuffer editedStory = new StringBuffer();
+		while (file.hasNextLine()) {
+			editedStory.append(file.nextLine());
+		}
+
+		for (String s : words) {
+			if (editedStory.toString().contains("<") && editedStory.toString().contains(">"))
+				editedStory.replace(editedStory.indexOf("<"), editedStory.indexOf(">"), s);
+		}
+		kb.close();
+
+		return new String(editedStory.toString());
+
+	}
+
+	public static void writeStoryArg(Scanner file3, Scanner file4) {
+		// TODO Auto-generated method stub
+
+	}
 }
